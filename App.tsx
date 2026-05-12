9966238c326815ecfd94310a4e61f794ef536f76
Dollah <sayedsajeedabdullah@gmail.com>
2026-05-12 09:31:21 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,49 @@
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import { Navbar } from "@/components/layout/navbar";
+
+import Home from "@/pages/home";
+import About from "@/pages/about";
+import Team from "@/pages/team";
+import Careers from "@/pages/careers";
+import Apply from "@/pages/apply";
+import Courses from "@/pages/courses";
+
+const queryClient = new QueryClient();
+
+function Router() {
+  return (
+    <div className="flex flex-col min-h-[100dvh]">
+      <Navbar />
+      <main className="flex-1">
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/team" component={Team} />
+          <Route path="/careers" component={Careers} />
+          <Route path="/apply" component={Apply} />
+          <Route path="/courses" component={Courses} />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+          <Router />
+        </WouterRouter>
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
